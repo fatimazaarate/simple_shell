ca68fd93881d22e47869e34fbe77a76c44f205ed
raziel362362 <lbt_alh@hotmail.fr>
2023-09-19 12:02:40 +0100

    tests

--- tests/getpath.c
+++ tests/getpath.c
@@ -34,8 +34,10 @@ char *_getenv(const char *name)
 {
 	extern char **environ;
 	int i = 0;
-	while (environ[i] != NULL) {
-		if (strncmp(name, environ[i], strlen(name)) == 0) {
+	while (environ[i] != NULL)
+	{
+		if (strncmp(name, environ[i], strlen(name)) == 0)
+		{
 			return environ[i] + strlen(name) + 1;
 		}
 		i++;
